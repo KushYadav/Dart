18bec73e30fc7f273b7f833242bd65d98b8bd06e
KushYadav <161220019@nitdelhi.ac.in>
2019-01-07 00:24:00 +0530

    Print and Comments

---
+++ bin/Started_01.dart
@@ -0,0 +1,11 @@
+main(List<String> args) {
+  print("Hello World");
+  print('c');
+  print(5);
+  print(true);
+  print(4 * 5);
+  // This is a comment
+  /*     
+   This is also a comment
+  */
+}
